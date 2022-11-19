b82ec873885af55f0324643b5357a4dac3ffb5b2
quanglong <tql0928159331@gmail.com>
2022-11-19 20:35:35 +0700

    19/11/2022

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -67,6 +67,19 @@ function Button({
 
 Button.propTypes = {
     children: PropTypes.node.isRequired,
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    rounded: PropTypes.bool,
+    text: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    className: PropTypes.string,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
 };
 
 export default Button;
